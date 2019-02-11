dec3c5c95c3089e0cb3d14467544518f4681249e
sarah.krabs <sarah.krabs@7p-group.com>
2019-02-11 14:49:22 +0100

    add getUserDataById

--- sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
+++ sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
@@ -3,7 +3,12 @@ package propra2.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import propra2.database.UserRepository;
+import propra2.model.User;
+
+import java.util.Optional;
 
 @Controller
 public class SharingIsCaringController {
@@ -13,4 +18,10 @@ public class SharingIsCaringController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @GetMapping("/profile/{customerId}")
+    public User getUserDataById(@PathVariable Long customerId){
+        Optional<User> user = userRepository.findById(customerId);
+        return user.get();
+    }
 }
