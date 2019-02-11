bba987278efe85e5690c4ef9ba2913e55295a304
sarah.krabs <sarah.krabs@7p-group.com>
2019-02-11 14:45:43 +0100

    add repositorys

--- sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
+++ sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
@@ -1,8 +1,16 @@
 package propra2.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import propra2.database.UserRepository;
 
 @Controller
 public class SharingIsCaringController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ProductRepository productRepository;
 }
