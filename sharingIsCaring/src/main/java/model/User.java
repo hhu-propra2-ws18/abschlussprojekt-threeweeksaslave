e08e1ea6cfb4aa45d7eb090fa878ba346536f557
commanderchristopher <christopher.orlowicz@hhu.de>
2019-02-11 14:00:02 +0100

    Creat user

---
+++ sharingIsCaring/src/main/java/model/User.java
@@ -0,0 +1,23 @@
+package model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long customerId;
+    private String username;
+    private String mail;
+    private String proPay;
+    private List<Product> productsToRent;
+    private List<Product> rentedProducts;
+
+}
