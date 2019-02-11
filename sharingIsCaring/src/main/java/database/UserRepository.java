fd179890ba4b0650011ae5ff015d47c5691a1dc2
commanderchristopher <christopher.orlowicz@hhu.de>
2019-02-11 14:05:55 +0100

    Creat user repository

---
+++ sharingIsCaring/src/main/java/database/UserRepository.java
@@ -0,0 +1,11 @@
+package database;
+
+import model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User,Long> {
+
+    List<User> findAll();
+}
