729043090134e225d152da2f7287dd301a8c76aa
sarah.krabs <sarah.krabs@7p-group.com>
2019-02-11 14:54:56 +0100

    add updateUserData

--- sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
+++ sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
@@ -1,8 +1,31 @@
 package propra2.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import propra2.database.UserRepository;
+import propra2.model.User;
+
+import java.util.Optional;
 
 @Controller
 public class SharingIsCaringController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/profile/{customerId}")
+    public User getUserDataById(@PathVariable Long customerId){
+        Optional<User> user = userRepository.findById(customerId);
+        return user.get();
+    }
+
+    @PostMapping("/profile/{customerId}")
+    public void updateUserData(@PathVariable Long customerId, @RequestBody User user){
+        userRepository.save(user);
+    }
 }
