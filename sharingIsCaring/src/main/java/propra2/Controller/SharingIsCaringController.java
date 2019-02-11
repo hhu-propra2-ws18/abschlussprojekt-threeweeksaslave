716aceafb0e45536483ad2dcb07823e4cb2b49a8
lebal101 <44257289+lebal101@users.noreply.github.com>
2019-02-11 15:09:39 +0100

    add Postmapping for user-registration + product-creation

--- sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
+++ sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
@@ -3,7 +3,11 @@ package propra2.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import propra2.database.UserRepository;
+import propra2.model.Product;
+import propra2.model.User;
 
 @Controller
 public class SharingIsCaringController {
@@ -13,4 +17,20 @@ public class SharingIsCaringController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @PostMapping("/registration")
+    String createUser(User newUser){
+        userRepository.save(newUser);
+        return "";
+    }
+
+    @PostMapping("/product")
+    String createProduct(Product newProduct){
+        User owner = userRepository.findById(newProduct.getOwnerId()).get();
+        owner.addProductToLend(newProduct);
+        userRepository.save(owner);
+        productRepository.save(newProduct);
+
+        return "";
+    }
 }
