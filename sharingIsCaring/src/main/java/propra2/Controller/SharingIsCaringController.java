b6d93a3babc068f085bda81ba0c1a98e4e390142
tiric103 <tim.richter99@web.de>
2019-02-11 14:52:21 +0100

    create Product PostMappings in controller

--- sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
+++ sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
@@ -1,8 +1,32 @@
 package propra2.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import propra2.database.ProductRepository;
+import propra2.database.UserRepository;
+import propra2.model.Product;
+
+import java.util.List;
 
 @Controller
 public class SharingIsCaringController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping
+    List<Product> searchForProducts(String productName){
+        List<Product> resultList = productRepository.findByTitle(productName);
+        return resultList;
+    }
+
+    @PostMapping
+    Product getProductInformationById(Long id){
+        return productRepository.findById(id).get();
+    }
 }
