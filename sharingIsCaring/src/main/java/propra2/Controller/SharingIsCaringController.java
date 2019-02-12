0826d27a9f624b4131888d3bd544ca8109a0d520
Regine Langen <regine@Regines-MacBook-Pro.local>
2019-02-12 13:54:27 +0100

    Added getOffers and getOrders to Controller

--- sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
+++ sharingIsCaring/src/main/java/propra2/Controller/SharingIsCaringController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import propra2.model.Customer;
+import propra2.model.OrderProcess;
 import propra2.repositories.CustomerRepository;
 import propra2.model.Product;
 import propra2.repositories.ProductRepository;
@@ -20,20 +21,20 @@ public class SharingIsCaringController {
 
     @Autowired
     ProductRepository productRepository;
-    
+
     @Autowired
     CustomerRepository customerRepository;
 
 
     @PostMapping("/registration")
-    String createUser(Customer newCustomer){
+    String createUser(Customer newCustomer) {
         customerRepository.save(newCustomer);
         return "";
     }
 
     @PostMapping("/product")
     String createProduct(Product newProduct) {
-        if(newProduct.allValuesSet()){
+        if (newProduct.allValuesSet()) {
             productRepository.save(newProduct);
         }
 
@@ -41,32 +42,47 @@ public class SharingIsCaringController {
     }
 
     @PostMapping("/product/{name}")
-    List<Product> searchForProducts(String name){
+    List<Product> searchForProducts(String name) {
         List<Product> resultList = productRepository.findByTitle(name);
         return resultList;
     }
 
     @PostMapping("/product/{id}")
-    Product getProductInformationById(Long id){
+    Product getProductInformationById(Long id) {
         return productRepository.findById(id).get();
     }
 
 
-    @PostMapping("/") 
-    public boolean userExists(final String username){
-        if(customerRepository.findByUsername(username).isPresent())
+    @PostMapping("/")
+    public boolean userExists(final String username) {
+        if (customerRepository.findByUsername(username).isPresent())
             return true;
-    throw new IllegalArgumentException();
+        throw new IllegalArgumentException();
     }
 
     @GetMapping("/profile/{customerId}")
-    public Customer getUserDataById(@PathVariable Long customerId){
+    public Customer getUserDataById(@PathVariable Long customerId) {
         Optional<Customer> user = customerRepository.findById(customerId);
         return user.get();
-    } 
-  
+    }
+
     @PostMapping("/profile/{customerId}")
-    public void updateUserData(@PathVariable Long customerId, @RequestBody Customer customer){
+    public void updateUserData(@PathVariable Long customerId, @RequestBody Customer customer) {
         customerRepository.save(customer);
     }
+
+    @GetMapping("/profile/offers/{customerId}")
+    public List<Product> getOffers(@PathVariable Long customerId) {
+        List<Product> products = productRepository.findByOwnerId(customerId);
+        return products;
+    }
+
+    @GetMapping("/profile/orders/{customerId}")
+    public List<Product> getOrders(@PathVariable Long customerId) {
+        Optional<Customer> customer = customerRepository.findById(customerId);
+        List<Product> products = productRepository.findAllByProductId(customer.get().getBorrowedProductIds());
+        return products;
+    }
+
+
 }
