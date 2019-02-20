a74ab27565f31e41d96549c3a261895212ebfc4d
lebal101 <44257289+lebal101@users.noreply.github.com>
2019-02-20 10:54:39 +0100

    copy files from spring tutorial
    Co-Authored-By: vinbru <vinbru@users.noreply.github.com>
    Co-Authored-By: selan103 <selan103@users.noreply.github.com>

--- sharingIsCaring/src/main/java/propra2/SharingIsCaringApplication.java
+++ sharingIsCaring/src/main/java/propra2/SharingIsCaringApplication.java
@@ -1,14 +1,28 @@
 package propra2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import propra2.storage.StorageProperties;
+import propra2.storage.StorageService;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties(StorageProperties.class)
 public class SharingIsCaringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SharingIsCaringApplication.class, args);
     }
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
+
---
+++ sharingIsCaring/src/main/java/propra2/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package propra2.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
