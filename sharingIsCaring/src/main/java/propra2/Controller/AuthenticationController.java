53f7691c0fe726527a59c85b66848d5498f9c87b
sarah.krabs <sarah.krabs@7p-group.com>
2019-02-22 10:59:19 +0100

    change layout home

--- sharingIsCaring/src/main/java/propra2/Controller/AuthenticationController.java
+++ sharingIsCaring/src/main/java/propra2/Controller/AuthenticationController.java
@@ -14,6 +14,8 @@ import propra2.model.UserRegistration;
 import propra2.repositories.CustomerRepository;
 import propra2.repositories.NotificationRepository;
 
+import java.io.File;
+import java.net.URL;
 import java.security.Principal;
 import java.util.List;
 
