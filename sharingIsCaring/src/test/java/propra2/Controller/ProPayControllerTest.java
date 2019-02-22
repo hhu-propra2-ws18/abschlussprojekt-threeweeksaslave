f65e8100c4351bfed5160798032cc1f459c40943
sarah.krabs <sarah.krabs@7p-group.com>
2019-02-22 11:02:25 +0100

    add ProPayControllerTest

---
+++ sharingIsCaring/src/test/java/propra2/Controller/ProPayControllerTest.java
@@ -0,0 +1,12 @@
+package propra2.Controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+@ContextConfiguration
+public class ProPayControllerTest {
+}
