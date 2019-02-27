5ece7b2b454f2f24a0b08627f6665c598b549ef7
lebal101 <44257289+lebal101@users.noreply.github.com>
2019-02-27 14:56:37 +0100

    add FileUploadTests (not working)
    Co-Authored-By: selan103 <selan103@users.noreply.github.com>
    Co-Authored-By: vinbru <vinbru@users.noreply.github.com>

---
+++ sharingIsCaring/src/test/java/propra2/storage/FileUploadTests.java
@@ -0,0 +1,54 @@
+package propra2.storage;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class FileUploadTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private StorageService storageService;
+
+    @Test
+    public void shouldSaveUploadedFile() throws Exception {
+        MockMultipartFile multipartFile = new MockMultipartFile("file", "test.png",
+                "text/plain", "Spring Framework".getBytes());
+        this.mvc.perform(fileUpload("/").file(multipartFile))
+                .andExpect(status().isFound())
+                .andExpect(header().string("Location", "/"));
+
+        then(this.storageService).should().store(multipartFile,"test.png", 1L);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void should404WhenMissingFile() throws Exception {
+        given(this.storageService.loadAsResource("test.png",1L))
+                .willThrow(StorageFileNotFoundException.class);
+
+        this.mvc.perform(get("/files/test.txt")).andExpect(status().isNotFound());
+    }
+
+}
