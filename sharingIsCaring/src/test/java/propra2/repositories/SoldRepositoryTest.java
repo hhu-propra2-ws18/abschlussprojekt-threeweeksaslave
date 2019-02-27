fae3b79ddc89b88968cbe0e641675680dd581e2d
sarah.krabs <sarah.krabs@7p-group.com>
2019-02-27 13:49:50 +0100

    ad SoldRepositoryTest

--- sharingIsCaring/src/test/java/propra2/repositories/SoldRepositoryTest.java
+++ sharingIsCaring/src/test/java/propra2/repositories/SoldRepositoryTest.java
@@ -1,4 +1,32 @@
 package propra2.repositories;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import propra2.database.Product;
+import reactor.test.StepVerifier;
+
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class SoldRepositoryTest {
+
+    @Autowired
+    SoldProductRepository soldProductRepository;
+
+    @Test
+    public void testFindById(){
+        Product product = new Product();
+        product.setId(1L);
+        product.setDailyFee(10);
+
+        soldProductRepository.save(product);
+        Optional<Product> product1 = soldProductRepository.findById(product.getId());
+
+        Assertions.assertThat(product1.get().getDailyFee()).isEqualTo(10);
+    }
 }
