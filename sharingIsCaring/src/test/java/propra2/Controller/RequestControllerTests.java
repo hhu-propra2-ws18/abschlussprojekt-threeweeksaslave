0e1b9bbb4dc89e406237fec2706fbf087097a394
sarah.krabs <sarah.krabs@7p-group.com>
2019-02-28 10:09:52 +0100

    add RequestControllerTests

--- sharingIsCaring/src/test/java/propra2/Controller/RequestControllerTests.java
+++ sharingIsCaring/src/test/java/propra2/Controller/RequestControllerTests.java
@@ -9,6 +9,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,9 +18,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import propra2.Security.service.CustomerService;
 import propra2.Security.service.RegistrationService;
 import propra2.Security.validator.CustomerValidator;
-import propra2.database.Customer;
-import propra2.database.OrderProcess;
-import propra2.database.Product;
+import propra2.database.*;
 import propra2.handler.OrderProcessHandler;
 import propra2.handler.SearchProductHandler;
 import propra2.handler.UserHandler;
@@ -34,6 +33,7 @@ import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static propra2.model.OrderProcessStatus.ACCEPTED;
 import static propra2.model.OrderProcessStatus.PENDING;
 
@@ -98,11 +98,14 @@ public class RequestControllerTests {
         kevin.setUsername("Kevin");
         kevin.setMail("kevin@istdumm.de");
         kevin.setPassword("Baumhaus");
+        ProPayAccount proPayAccount = new ProPayAccount();
+        kevin.setProPay(proPayAccount);
 
         product1.setTitle("Baumstamm");
         product1.setId(34L);
         product1.setAvailable(false);
         product1.setOwner(kevin);
+        product1.setDailyFee(10);
 
         product2.setTitle("Baumlaube");
         product2.setId(56L);
@@ -152,6 +155,87 @@ public class RequestControllerTests {
                         hasProperty("username", is("Kevin")),
                         hasProperty("mail", is("kevin@istdumm.de")))));
     }
+
+
+    @Test
+    @WithMockUser(username = "Zoidberg", password = "propra2")
+    public void testCancelOrder() throws Exception{
+        OrderProcess process1 = new OrderProcess();
+        process1.setId(13L);
+        process1.setOwnerId(2L);
+        process1.setProduct(product1);
+        process1.setRequestId(111L);
+        process1.setStatus(PENDING);
+        List<OrderProcess> owner = new ArrayList();
+        owner.add(process1);
+
+        Mockito.when(orderProcessRepository.findById(13L)).thenReturn(java.util.Optional.of(process1));
+        Mockito.when(customerRepository.findByUsername("Zoidberg")).thenReturn(java.util.Optional.of(bendisposto));
+        Mockito.when(orderProcessHandler.updateOrderProcess(new ArrayList<>(), process1)).thenReturn(true);
+
+        mvc.perform(post("/requests/detailsBorrower/13")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("action", "cancel"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("requests"));
+    }
+
+    @Test
+    @WithMockUser(username = "Zoidberg", password = "propra2")
+    public void testDeleteByBorrower() throws Exception{
+
+        OrderProcess process1 = new OrderProcess();
+        process1.setId(13L);
+        process1.setOwnerId(2L);
+        process1.setProduct(product1);
+        process1.setRequestId(111L);
+        process1.setStatus(PENDING);
+        List<OrderProcess> owner = new ArrayList();
+        owner.add(process1);
+
+        Mockito.when(orderProcessRepository.findById(13L)).thenReturn(java.util.Optional.of(process1));
+        Mockito.when(customerRepository.findByUsername("Zoidberg")).thenReturn(java.util.Optional.of(bendisposto));
+
+        mvc.perform(post("/requests/detailsBorrower/13")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("action", "delete"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/requests"));
+    }
+
+
+    @Test
+    @WithMockUser(username = "Zoidberg", password = "propra2")
+    public void testReturnProduct() throws Exception{
+
+        OrderProcess process1 = new OrderProcess();
+        process1.setProduct(product1);
+        process1.setFromDate(new java.sql.Date(System.currentTimeMillis()));
+        process1.setId(13L);
+        process1.setOwnerId(2L);
+        process1.setProduct(product1);
+        process1.setRequestId(111L);
+        process1.setStatus(PENDING);
+        List<OrderProcess> owner = new ArrayList();
+        owner.add(process1);
+
+        Notification notification = new Notification();
+        notification.setProcessId(13L);
+
+        Mockito.when(orderProcessRepository.findById(13L)).thenReturn(java.util.Optional.of(process1));
+        Mockito.when(customerRepository.findByUsername("Zoidberg")).thenReturn(java.util.Optional.of(bendisposto));
+        Mockito.when(orderProcessHandler.payDailyFee(process1)).thenReturn(true);
+        Mockito.when(customerRepository.findById(111L)).thenReturn(java.util.Optional.of(kevin));
+        Mockito.when(customerRepository.findById(2L)).thenReturn(java.util.Optional.of(bendisposto));
+        Mockito.when(notificationRepository.findByProcessId(13L)).thenReturn(java.util.Optional.of(notification));
+
+        mvc.perform(post("/requests/detailsBorrower/13")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("action", "return"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/requests"));
+    }
+
 
     @Test
     @WithMockUser(username = "Zoidberg", password = "propra2")
@@ -307,6 +391,120 @@ public class RequestControllerTests {
                         hasProperty("id", is(34L)))));
 
 
+    }
+
+
+    @Test
+    @WithMockUser(username = "Zoidberg", password = "propra2")
+    public void testAcceptProcess() throws Exception{
+        OrderProcess process = new OrderProcess();
+        process.setId(13L);
+        process.setOwnerId(2L);
+        process.setProduct(product1);
+        process.setRequestId(111L);
+        process.setStatus(PENDING);
+        ArrayList<Message> messages = new ArrayList<>();
+        process.setMessages(messages);
+
+        Mockito.when(orderProcessRepository.findById(13L)).thenReturn(java.util.Optional.of(process));
+        Mockito.when(productRepository.findById(34L)).thenReturn(java.util.Optional.of(product1));
+        Mockito.when(orderProcessHandler.updateOrderProcess(messages, process)).thenReturn(true);
+
+        mvc.perform(post("/requests/detailsLender/13")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("action", "acceptProcess"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/requests"));
+    }
+
+    @Test
+    @WithMockUser(username = "Zoidberg", password = "propra2")
+    public void testFinishProcess() throws Exception{
+        OrderProcess process = new OrderProcess();
+        process.setId(13L);
+        process.setOwnerId(2L);
+        process.setProduct(product1);
+        process.setRequestId(111L);
+        process.setStatus(PENDING);
+        ArrayList<Message> messages = new ArrayList<>();
+        process.setMessages(messages);
+
+        Mockito.when(orderProcessRepository.findById(13L)).thenReturn(java.util.Optional.of(process));
+        Mockito.when(customerRepository.findById(111L)).thenReturn(java.util.Optional.of(kevin));
+        Mockito.when(productRepository.findById(34L)).thenReturn(java.util.Optional.of(product1));
+        Mockito.when(orderProcessHandler.updateOrderProcess(messages, process)).thenReturn(true);
+
+        mvc.perform(post("/requests/detailsLender/13")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("action", "acceptReturn"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/requests"));
+    }
+
+    @Test
+    @WithMockUser(username = "Zoidberg", password = "propra2")
+    public void testAppealProcess() throws Exception{
+        OrderProcess process = new OrderProcess();
+        process.setId(13L);
+        process.setOwnerId(2L);
+        process.setProduct(product1);
+        process.setRequestId(111L);
+        process.setStatus(PENDING);
+        ArrayList<Message> messages = new ArrayList<>();
+        process.setMessages(messages);
+
+        Mockito.when(orderProcessRepository.findById(13L)).thenReturn(java.util.Optional.of(process));
+        Mockito.when(orderProcessHandler.updateOrderProcess(messages, process)).thenReturn(true);
+
+        mvc.perform(post("/requests/detailsLender/13")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("action", "appeal")
+                .requestAttr("message", "hallo"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/requests"));
+    }
+
+    @Test
+    @WithMockUser(username = "Zoidberg", password = "propra2")
+    public void testDeleteByOwner() throws Exception{
+        OrderProcess process = new OrderProcess();
+        process.setId(13L);
+        process.setOwnerId(2L);
+        process.setProduct(product1);
+        process.setRequestId(111L);
+        process.setStatus(PENDING);
+        ArrayList<Message> messages = new ArrayList<>();
+        process.setMessages(messages);
+
+        Mockito.when(orderProcessRepository.findById(13L)).thenReturn(java.util.Optional.of(process));
+
+        mvc.perform(post("/requests/detailsLender/13")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("action", "deleteProcess"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/requests"));
+    }
+
+    @Test
+    @WithMockUser(username = "Zoidberg", password = "propra2")
+    public void testDenyProcess() throws Exception{
+        OrderProcess process = new OrderProcess();
+        process.setId(13L);
+        process.setOwnerId(2L);
+        process.setProduct(product1);
+        process.setRequestId(111L);
+        process.setStatus(PENDING);
+        ArrayList<Message> messages = new ArrayList<>();
+        process.setMessages(messages);
+
+        Mockito.when(orderProcessRepository.findById(13L)).thenReturn(java.util.Optional.of(process));
+        Mockito.when(orderProcessHandler.updateOrderProcess(messages, process)).thenReturn(true);
+
+        mvc.perform(post("/requests/detailsLender/13")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("action", "deny"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/requests"));
     }
 
 
