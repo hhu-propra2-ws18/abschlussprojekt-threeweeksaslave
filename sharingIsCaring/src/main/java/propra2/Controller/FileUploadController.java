73d62b9c45e0d95935e3276f64b52c584515987d
Vinbru <44266431+Vinbru@users.noreply.github.com>
2019-03-01 11:51:39 +0100

    deleted unnecessary comments

--- sharingIsCaring/src/main/java/propra2/Controller/FileUploadController.java
+++ sharingIsCaring/src/main/java/propra2/Controller/FileUploadController.java
@@ -1,9 +1,5 @@
 package propra2.Controller;
 
-import java.io.IOException;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import propra2.database.Product;
 import propra2.repositories.ProductRepository;
 import propra2.storage.StorageFileNotFoundException;
 import propra2.storage.StorageService;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/upload")
@@ -50,8 +47,6 @@ public class FileUploadController {
     @ResponseBody
     public Resource serveFile(@PathVariable String filename, @PathVariable Long productId) {
         Resource file = storageService.loadAsResource(filename, productId);
-        /*return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);*/
         return file;
     }
 
--- sharingIsCaring/src/main/java/propra2/storage/FileSystemStorageService.java
+++ sharingIsCaring/src/main/java/propra2/storage/FileSystemStorageService.java
@@ -1,14 +1,5 @@
 package propra2.storage;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.stream.Stream;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -16,6 +7,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileSystemStorageService implements StorageService {
@@ -69,8 +68,6 @@ public class FileSystemStorageService implements StorageService {
 				return runningCurrentRootLocation;
 			}
 		}
-		/*currentRootLocation = Paths.get("src/main/resources/static/img");
-		return currentRootLocation.resolve("dummyProductPicture.JPG");*/
 		return  null;
 
     }
